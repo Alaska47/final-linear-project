9d7550f4c9841d54275e56a485b61c6fb8de12c7
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2017-05-10 19:53:07 -0400

    add demo pics

---
+++ demo.sh
@@ -0,0 +1,11 @@
+
+python lin_proj.py demo.png demo_1_IDENTITY.png identity 1
+python lin_proj.py demo.png demo_2_SHARPEN.png sharpen 1
+python lin_proj.py demo.png demo_3_BLUR.png blur 3
+python lin_proj.py demo.png demo_4_BOTTOM_EDGE.png bottom_edge 1
+python lin_proj.py demo.png demo_5_TOP_EDGE.png top_edge 1
+python lin_proj.py demo.png demo_6_LEFT_EDGE.png left_edge 1
+python lin_proj.py demo.png demo_7_RIGHT_EDGE.png right_edge 1
+python lin_proj.py demo.png demo_8_EMBOSS.png emboss 1
+
+rm -rf demo.png
