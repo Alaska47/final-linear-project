f95ebb0904f43e8e575ef6932a7cb3272c662f63
Aneesh Kotnana <aneesh.kotnana@gmail.com>
2017-05-10 20:29:49 -0400

    test1

--- do.sh
+++ do.sh
@@ -1,11 +1,21 @@
 rm -rf output*.png
 
-convert IMG*.JPG -rotate 90 output.png
+convert IMG*.JPG -rotate 90 -resize 75% output.png
 rm -rf IMG*.JPG
 
-python lin_proj.py output.png output_1.png identity 1
-python lin_proj.py output.png output_2.png sharpen 1
-python lin_proj.py output.png output_3.png blur 5
-python lin_proj.py output.png output_4.png edge 1
+python lin_proj.py output.png output_1_IDENTITY.png identity 1
+python lin_proj.py output.png output_2_SHARPEN.png sharpen 1
+python lin_proj.py output.png output_3_BLUR.png blur 3
+python lin_proj.py output.png output_4_BOTTOM_EDGE.png bottom_edge 1
+python lin_proj.py output.png output_5_TOP_EDGE.png top_edge 1
+python lin_proj.py output.png output_6_LEFT_EDGE.png left_edge 1
+python lin_proj.py output.png output_7_RIGHT_EDGE.png right_edge 1
+python lin_proj.py output.png output_8_EMBOSS.png emboss 1
+
+git add output*
+
+git commit -m "add example"
+
+git push origin master
 
 rm -rf output.png
